143d8d1c8736d577d103a8cb16dccbaecf6e5dae
yeyoon <875776209@qq.com>
2018-05-10 23:34:58 +0800

    add push/pop gen asm codes

--- ch7/vm_translate.lisp
+++ ch7/vm_translate.lisp
@@ -4,6 +4,33 @@
 ;; version: 1.0.0
 ;;
 ;; vm translator
+(defvar *stack-top* 256)
+
+(defun gen-push-asm (segment index)
+    (with-output-to-string (out)
+        (write-line (concatenate 'string "@" (write-to-string (+ segment index))))
+        (write-line "D=M")
+        (write-line (concatenate 'string "@" (write-to-string *stack-top*)))
+        (setf *stack-top* (+ 1 *stack-top*))
+        (write-string "M=D")))
+
+(defun gen-pop-asm (segment index)
+    (with-output-to-string (out)
+        (setf *statck-top* (- *stack-top* 1))
+        (write-line (concatenate 'string "@" (write-to-string *stack-top*)))
+        (write-line "D=M")
+        (write-line (concatenate 'string "@" (write-to-string (+ segment index))))
+        (write-string "M=D")))
+
+(defun gen-normal-asm (op)
+    (format T "noraml operation op : ~a~%" op))
+
+(defun genasm (op &rest args)
+    "The basic function to use gen asm codes."
+    (cond ((push? op) (gen-push-asm (first args) (second args)))
+          ((pop? op) (gen-pop-asm (first args) (second args)))
+          (T (gen-normal-asm op))))
+          
 
 (defun statck-op (op segment-or-var &optional index)
     "The Stack push/pop implementation.
@@ -14,11 +41,11 @@
     (let ((top 0))
         (if (string= op "push")
             (progn
-                (translate-push segment-or-var index top)
+                (genasm 'push segment-or-var index top)
                 (setf top (+ 1 top)))
             (progn
                 (setf top (- top 1))
-                (translate-pop segment-or-var top)))))
+                (genasm 'pop segment-or-var top)))))
                 
         
     
